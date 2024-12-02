71cbd6d543b60323f884c96ae9e960cdfb129e55
yakovlevav <yakovlevav@txix.ru>
2024-12-02 05:32:45 +0300

    add tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,4 +1,4 @@
-import { PostsService } from './posts.service';
+import {Post, PostsService} from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
@@ -20,13 +20,35 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const resPosts: Post[] = [
+        {id: '1', text: 'Post 1'},
+        {id: '2', text: 'Post 2'},
+        {id: '3', text: 'Post 3'},
+        {id: '4', text: 'Post 4'},
+      ]
+      expect(postsService.findMany()).toEqual(resPosts)
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const resPosts: Post[] = [
+        {id: '3', text: 'Post 3'},
+        {id: '4', text: 'Post 4'},
+      ];
+      expect(postsService.findMany({ skip: 2, limit: 2 })).toEqual(resPosts);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return an empty array if skip is greater than the number of posts', () => {
+      expect(postsService.findMany({ skip: 5 })).toEqual([]);
+    });
+
+    it('should return all posts if limit exceeds the total number of posts', () => {
+      const resPosts: Post[] = [
+        {id: '1', text: 'Post 1'},
+        {id: '2', text: 'Post 2'},
+        {id: '3', text: 'Post 3'},
+        {id: '4', text: 'Post 4'},
+      ];
+      expect(postsService.findMany({ limit: 10 })).toEqual(resPosts);
+    });
   });
 });
